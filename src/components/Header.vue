c08e12c9fa761c2bb418fb1f953cb5a37fc65c97
Laura <lauragarciacastelao@hotmail.com>
2022-07-20 15:47:31 +0200

    Create changes

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<script>
+</script>
+
+<template>
+<header>
+        <h1> EDUCODERS </h1>
+</header>
+
+</template>
+
+
+<style lang="css">
+
+
+header{
+    background-color: #F9D616;
+    height: 8vh;
+    
+    padding-bottom: 10vh;
+    margin:0;
+}
+   
+h1 {
+  height: 60px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  font-family:'Times New Roman', Times, serif;
+  font-size: 5vh;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import Header from "../components/Header.vue";
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+
+
+<Header/>
+
 </template>
+
+<style scoped>
+
+</style>
+
+
