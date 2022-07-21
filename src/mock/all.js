1c5e704fa70ef4332d2037a0fd01d3526c46ff27
mada <madaagarcia77@gmail.com>
2022-07-21 09:56:52 +0200

    agregar variable booleana en json

--- src/mock/all.js
+++ src/mock/all.js
@@ -1,21 +1,20 @@
 
 const advertisers = [
   {
-    id: 0,
+    id: 1,
     name: 'Full Stack',
-    description:
-      'Conviértete en un desarrollador web full stack. Aprende diseño Front-end y arquitectura Back-end además de las tecnologías más punteras. Aprenderás a programar a partir de los fundamentos de HTML5, CSS3, JavaScript, Node.js, Express, MongoDB y React. Puedes realizar este curso a tiempo completo o parcial.',
+    description:'Conviértete en un desarrollador web full stack. Aprende diseño Front-end y arquitectura Back-end además de las tecnologías más punteras. Aprenderás a programar a partir de los fundamentos de HTML5, CSS3, JavaScript, Node.js, Express, MongoDB y React. Puedes realizar este curso a tiempo completo o parcial.',
     duration: '500 hs',
     price: '$150',
     modality: 'Presencial',
     formador: 'Tania Rodriguez',
-    coformador: 'David Soto',
-    location: 'Madrid, Espana',
-    cover:
-      'https://www.noticias.ltda/wp-content/uploads/2019/02/Curso-online.png',
-  },
+    coformador: 'David St',
+    location: 'Madrid, sana',
+    cover:'https://www.noticias.ltda/wp-content/uploads/2019/02/Curso-online.png',
+    complete: false
+},
   {
-    id: '2',
+    id: 2,
     name: 'Marketing',
     description:
       'Aprenda Marketing Digital para crecer su negocio: Marketing de redes sociales, Facebook, SEO, YouTube, E-mail Marketing.',
@@ -25,10 +24,11 @@ const advertisers = [
     formador: 'Juan Fernandez',
     coformador: 'Marta Benitez',
     location: 'Medellin, Colombia',
-    cover: 'https://mastermarketingupv.com/blog-macom/13-titulos-profesionales-de-marketing-online-validos-en-todo-el-mundo-y-gratis/',
-  },
+    cover: 'https://mastermarketingupv.com/blog-macom/13-titulos-profesionales-de-marketing-online-validose-todo-el-mundo-y-gratis/',
+    complete: false
+},
   {
-    id: '3',
+    id: 3,
     name: 'Data Analytics',
     description:
       'vas a aprender como hacer informes y reportes con dashboards en distintos proyectos, ejercicios y prácticas',
@@ -39,12 +39,13 @@ const advertisers = [
     coformador: 'Sofia Perez',
     location: 'Barcelona, Espana',
     cover: 'https://freerangestock.com/photos/61056/data-analytics-concept.html',
-  },
+    complete: false
+},
   {
-    id: '4',
+    id: 4,
     name: 'Diseño UX/UI',
     description:
-      'Conviértete en un diseñador UX/UI. Aprende a realizar diseños en los que el usuario sea el protagonista gracias a el proceso de Design Thinking y valida tus ideas a través de los conceptos de user research, prototipado, user testing y la evaluación heurística. Puedes realizar este curso a tiempo completo o parcial.',
+      'Conviértete en un diseñador UX/UI. Aprende a realizar diseños en los que el usuario sea el protagonista gracias a el proceso de Design Thinking y valida u idea  través de los conceptos de user research, prototipado, user testing y la evaluación heurística. Puedes realizar este curso a tiempo completo o parcial.',
     duration: '250 hs',
     price: '$200',
     modality: 'on-line',
@@ -52,9 +53,10 @@ const advertisers = [
     coformador: 'Macarena Zanches',
     location: 'Buens Aires, Argenitna',
     cover: 'https://www.marketinhouse.es/diseno-ux-ui/',
-  },
+    complete: false
+},
   {
-    id: '5',
+    id: 5,
     name: 'Python TOTAL',
     description:
       'Aplicarás Python en aplicaciones como: Juegos, Inteligencia Artificial, Machine Learning, Data Science, Gestión Administrativa y mucho más',
@@ -65,9 +67,10 @@ const advertisers = [
     coformador: 'Facundo Zetta',
     location: 'Valencia, Espana',
     cover: 'https://www.toptal.com/python/an-introduction-to-mocking-in-python',
-  },
+    complete: false
+},
   {
-    id: '6',
+    id: 6,
     name: 'Angular',
     description: 'Crear aplicaciones en Angular siguiendo las mejores prácticas. Dominar TypeScript para crear aplicaciones en Angular',
     duration: '600 hs',
@@ -77,9 +80,10 @@ const advertisers = [
     coformador: 'Danilo Benitez',
     location: 'Medellin, Colombia',
     cover: 'https://www.comunicae.es/nota/curso-desarrollo-web-mediante-lenguajes-guion-1232412/',
-  },
+    complete: false
+},
   {
-    id: '7',
+    id: 7,
     name: 'PHP',
     description: 'Aplicará los diferentes tipos de datos que se manejan en PHP y las reglas generales para nombrar las variables.',
     duration: '200 hs',
@@ -89,9 +93,10 @@ const advertisers = [
     coformador: 'Maria Luchini',
     location: 'Lima, Peru',
     cover: 'https://rockcontent.com/es/blog/php/',
-  },
+    complete: false
+},
   {
-    id: '8',
+    id: 8,
     name: 'Bases de datos (SQL)',
     description: 'este curso debería ser tomado no solo por aquellos que no posean conocimientos sobre bases de datos, sino por cada profesional que desee optimizar al máximo las bases de datos con las que trabaja.',
     duration: '250 hs',
@@ -99,28 +104,30 @@ const advertisers = [
     modality: 'presencial',
     formador: 'Marta Lorenzon',
     coformador: 'Viola Frutnes',
-    location: 'Madrid, Espana',
+    location: 'Madrid, sana',
     cover: 'https://www.certus.edu.pe/blog/que-es-sql/',
-  },
+    complete: false
+},
   {
-    id: '9',
+    id: 9,
     name: 'Git y GitHub desde Cero a Experto',
-    description: 'Sistema de control de versiones Git y GitHub desde cero, de manera rápida, bien explicada y 100% Actualizado!',
+    description: 'Sistema de control de versiones Git y GitHub desde cero, de manera rápida,ben explicada y 100% Actualizado!',
     duration: '230 hs',
     price: '$180',
     modality: 'presencial',
     formador: 'Raul Manzano',
     coformador: 'Laura Soto',
-    location: 'Madrid, Espana',
+    location: 'Madrid, sana',
     cover: 'https://github.com/explore',
-  }
+    complete: false
+}
 ]
 
 export default advertisers;
 
 // ,
 //   {
-//     id: '',
+//     id: '
 //     name: '',
 //     description: '',
 //     duration: '',
@@ -130,4 +137,4 @@ export default advertisers;
 //     coformador: '',
 //     location: '',
 //     cover: '',
-//   }
+//  }
