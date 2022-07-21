269e788e95505d090a9b69b034071b3d70beadd5
David <jodasos03@gmail.com>
2022-07-21 09:40:51 +0200

    print to screen and send to favorites

--- src/mock/all.js
+++ src/mock/all.js
@@ -1,25 +1,42 @@
-const todos = [
-    {id_curso: 0,
-    name: "Full Stack",
-    description: "Conviértete en un desarrollador web full stack. Aprende diseño Front-end y arquitectura Back-end además de las tecnologías más punteras. Aprenderás a programar a partir de los fundamentos de HTML5, CSS3, JavaScript, Node.js, Express, MongoDB y React. Puedes realizar este curso a tiempo completo o parcial.",
-    duration: "500 hs",
-    price: "$150",
-    modality: "Presencial",
-    formador: "Tania Rodriguez",
-    coformador: "David Soto",
-    location: "Madrid, Espana",
-    cover: "https://www.noticias.ltda/wp-content/uploads/2019/02/Curso-online.png"},
+const advertisers = [
     {
-    id_curso: "",
-    name: "Marketing",
-    description: "Aprenda Marketing Digital para crecer su negocio: Marketing de redes sociales, Facebook, SEO, YouTube, E-mail Marketing.",
-    duration: "200 hs",
-    price: "$100",
-    modality: "on-line",
-    formador: "Juan Fernandez",
-    coformador: "Marta Benitez",
-    location: "Medellin, Colombia",
-    cover: "124565656"
+        id: 1,
+        name: "Full Stack",
+        description: "Conviértete en un desarrollador web full stack. Aprende diseño Front-end y arquitectura Back-end además de las tecnologías más punteras. Aprenderás a programar a partir de los fundamentos de HTML5, CSS3, JavaScript, Node.js, Express, MongoDB y React.",
+        duration: "500 hs",
+        price: "$150",
+        modality: "Presencial",
+        formador: "Tania Rodriguez",
+        coformador: "David Soto",
+        location: "Madrid, Espana",
+        cover: "https://www.noticias.ltda/wp-content/uploads/2019/02/Curso-online.png",
+        completed: false
+    },
+    {
+        id: 2,
+        name: "Marketing",
+        description: "Aprenda Marketing Digital para crecer su negocio: Marketing de redes sociales, Facebook, SEO, YouTube, E-mail Marketing y demas .",
+        duration: "200 hs",
+        price: "$100",
+        modality: "on-line",
+        formador: "Juan Fernandez",
+        coformador: "Marta Benitez",
+        location: "Medellin, Colombia",
+        cover: "124565656",
+        completed: false
+    },
+    {
+        id: 3,
+        name: "Javascript",
+        description: "Aprenda Marketing Digital para crecer su negocio: Marketing de redes sociales, Facebook, SEO, YouTube, E-mail Marketing y demas .",
+        duration: "1000 hs",
+        price: "$5000",
+        modality: "on-line",
+        formador: "luis Fernandez",
+        coformador: "Marta Benitez",
+        location: "Tierra, via lactea",
+        cover: "124565656",
+        completed: false    
     }
     /* {
     id_curso: "",
@@ -35,4 +52,4 @@ const todos = [
     } */
 ]
 
-export default todos;
+export default advertisers;
--- src/todosSetup.js
+++ src/todosSetup.js
@@ -1,18 +1,21 @@
 import { computed, ref } from "@vue/reactivity";
-import initialtodos from './mock/all.js';
+import initialadvertiser from './mock/all.js';
 
-const todos = ref([...initialtodos]);
-const todosIncompleted = computed(() => todos.value.filter(t => !t.completed));
-const todosCompleted = computed(() => todos.value.filter(t => t.completed));
+const advertisers = ref([...initialadvertiser]);
+const advertisersIncompleted = computed(() => advertisers.value.filter(t => !t.completed));
+const advertisersCompleted = computed(() => advertisers.value.filter(t => t.completed));
 
 const toggle = (id) => {
-    const todo = todos.value.find(t => t.id === id);
-    if (todo?.completed !== undefined) {
-        todo.completed = !todo.completed;
+    const advertiser = advertisers.value.find(t => t.id === id);
+    if (advertiser?.completed !== undefined) {
+        advertiser.completed = !advertiser.completed;
     }
 };
 
+const deleteAdvertiser = (id) => {
+    advertisers.value = advertisers.value.filter(t => t.id !== id);
+}
 
-const todosFactory = () => ({todosIncompleted, todosCompleted, toggle});
+const advertisersFactory = () => ({ advertisersIncompleted, advertisersCompleted, toggle, deleteAdvertiser });
 
-export {todosFactory};
+export { advertisersFactory }; 
