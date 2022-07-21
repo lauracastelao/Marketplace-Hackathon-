c20a832d7d20b1c4692f4ede7c4c4cdb0d216c06
matt sb <mattsandovalb@gmail.com>
2022-07-21 20:16:07 +0200

    matt-home-final

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -12,14 +12,12 @@ const picturesFind = addPicture.value.forEach((t)=> {
 </script>
 
 <template>
-  <div class="card" style="width: 18rem">
+  <div class="card container-fluid  border-0 " style="width: 18rem">
 
-   <!--  <img v-for="addPicture in addPictures" :key="addPicture.cover"
+  <!-- <img v-for="addPicture in addPictures" :key="addPicture.cover"
       :src="addPictures"
       class="card-img-top"
     > -->
-  
-
     <div
       class="card-body"
       v-for="advertiser in advertisersIncompleted"
@@ -39,45 +37,28 @@ const picturesFind = addPicture.value.forEach((t)=> {
 </template>
 
 <style lang="css" scoped>
-.cards2 {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 12%;
-}
 .card {
-  margin-top: 10vh;
-  width: 30vh;
-  height: 40vh;
-
+  display: flex;
+  margin-top: 12%;
+  align-items: center;  
 }
 
 .heart {
   display: flex;
   justify-content: right;
-}
-
-.button3 {
-  display: flex;
-  justify-content: center;
-}
-
-.heart {
   border: none;
   background-color: transparent;
 }
 
-.card-body {
-  padding: 7vh;
-  margin-bottom: 10vh;
-  width: 40vh;
-  box-shadow: 2px 2px 2px 2px black, 8px 8px 8px 5px ;
-}
 
-.universe {
-  width: 100%;
-  height: 10%;
+.btn {
+  display: flex;
+  justify-content: center;
 }
+.card-body {
+  padding: 20%;
+  margin-bottom: 20%;
+  width: 130%;
+  box-shadow: 2px 2px 2px 2px rgb(138, 138, 138);
+} 
 </style>
