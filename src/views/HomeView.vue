7900bd677bb5e245f7c5a575f398f6f61b5edcc1
matt sb <mattsandovalb@gmail.com>
2022-07-21 10:29:03 +0200

    matt-pull-develop

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,19 @@
 <script setup>
-<<<<<<< HEAD
-import Footer from "../components/Footer.vue";
-import Card from "../components/Card.vue";
-=======
->>>>>>> 1c5e704fa70ef4332d2037a0fd01d3526c46ff27
 
 import { advertisersFactory } from "../todosSetup";
 const { advertisersIncompleted, toggle } = advertisersFactory();
 </script>
 
 <template>
-<<<<<<< HEAD
+
 
 
 <Card/>
 <Header/>
-<Footer/>
-</template>
-=======
-  <h3>Cursos</h3>
-
-  <div class="grid">
-    <div
-      class="box"
+    <div>
       v-for="advertiser in advertisersIncompleted"
       :key="advertiser.id"
-    >
+    <div>
       <button @click="toggle(advertiser.id)">
         <a>
           <img
