ac5aa05658b4d370e6bf92c0a2ab291296ec9800
Raúl Torres <99418611+rauledutorres@users.noreply.github.com>
2022-07-21 18:23:25 +0200

    creacion rama raul

--- src/views/Member.vue
+++ src/views/Member.vue
@@ -3,20 +3,18 @@
 
 import { advertisersFactory } from "../todosSetup";
 const {add} = advertisersFactory();
-const addAndNavigate = (name,description) => {
-    add(name),
-    add(description)
-   
+/*const addAndNavigate = (name,description) => {
+    add(name,description);
   
-}
+}*/
 </script>
 
 <template>
 <div class="container">
-      <label for="new-task">New Member's</label>
-      <input id="new-task" type="text" v-model="name">
-       <input id="new-task" type="text" v-model="description"> 
-      <button type="add" @click="addAndNavigate(name, description)">Add</button>
+        <label for="new-task">New Member's</label>
+        <input id="new-task" type="text" v-model="name">
+        <input id="new-task" type="text" v-model="description"> 
+      <button type="add" @click="add(name, description)">Add</button>
   </div>
 
 </template>
