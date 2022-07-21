6953f7c1e355914beaed0774d895dbaf553c306d
matt sb <mattsandovalb@gmail.com>
2022-07-21 16:15:02 +0200

    matt-commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Card from '../components/Card.vue';
 
 
 </script>
@@ -6,42 +7,6 @@
 <template>
 
 
-<<<<<<< HEAD
-
-<Card/>
-<Header/>
-    <div>
-      v-for="advertiser in advertisersIncompleted"
-      :key="advertiser.id"
-    <div>
-      <button @click="toggle(advertiser.id)">
-        <a>
-          <img
-            class="icon-favorite"
-            src="../assets/img/fa-heart.svg"
-            alt="favorite"
-          />
-        </a>
-      </button>
-      <p>{{ advertiser.name }}</p>
-      <p>{{ advertiser.description }}</p>
-      <p>{{ advertiser.duration }}</p>
-      <p>{{ advertiser.price }}</p>
-      <p>{{ advertiser.modality }}</p>
-      <p>{{ advertiser.formador }}</p>
-      <p>{{ advertiser.coformador }}</p>
-      <p>{{ advertiser.location }}</p>
-      <img :src="advertisers" />
-      <br />
-    </div>
-  </div>
-</template>
-
-<style scoped>
-@import "../assets/card.css";
-</style>
->>>>>>> 1c5e704fa70ef4332d2037a0fd01d3526c46ff27
-=======
 <Card/>
 <Header/>
 <Footer/>
@@ -53,4 +18,3 @@
 <style scoped>
 @import "../assets/card.css";
 </style>
->>>>>>> 803c4fd62d4fcad0d648d741dba82f4b0a4cf694
